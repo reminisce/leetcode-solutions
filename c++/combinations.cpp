8a4a09919efb214821b6d2e11a495d33f1b8638a
reminisce <wujun.nju@gmail.com>
2016-04-25 22:48:27 -0700

    Replaced tabs with 4 spaces as indentation

--- c++/combinations.cpp
+++ c++/combinations.cpp
@@ -22,39 +22,39 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {      
-    	vector<vector<int>> rs;
-		vector<int> comb;
-		comb.reserve(k);
-		combine(1, n, k, rs, comb);
+        vector<vector<int>> rs;
+        vector<int> comb;
+        comb.reserve(k);
+        combine(1, n, k, rs, comb);
 
-    	return rs;
+        return rs;
     }
 
     void combine(int start, int n, int k, vector<vector<int>>& rs, vector<int>& comb) {
-    	if (k == 0) {
-    		rs.push_back(comb);
-    		return;
-    	}
+        if (k == 0) {
+            rs.push_back(comb);
+            return;
+        }
 
-    	for (int i = start; i <= n; ++i) {
-	    	comb.push_back(i);
-	    	combine(i+1, n, k-1, rs, comb);
-		    comb.pop_back();
-	    }
+        for (int i = start; i <= n; ++i) {
+            comb.push_back(i);
+            combine(i+1, n, k-1, rs, comb);
+            comb.pop_back();
+        }
     }
 };
 
 int main()
 {
-	Solution sol;
-	int n = 2;
-	int k = 1;
-	vector<vector<int>> rs = sol.combine(n, k);
-	for (vector<int>& r : rs) {
-		for (int v : r) {
-			cout << v << ' ';
-		}
-		cout << endl;
-	}
-	return 0;
+    Solution sol;
+    int n = 2;
+    int k = 1;
+    vector<vector<int>> rs = sol.combine(n, k);
+    for (vector<int>& r : rs) {
+        for (int v : r) {
+            cout << v << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
 }
