37dbad41ef577cb3609077e67e41efb5af5e9b16
reminisce <wujun.nju@gmail.com>
2016-04-25 22:56:33 -0700

    Replaced tabs with 4 spaces as indentation

--- c++/decode_ways.cpp
+++ c++/decode_ways.cpp
@@ -21,39 +21,39 @@ using namespace std;
 
 class Solution {
 public:
-	/**
-	 * Scan from the last char to the first and keep
-	 * tracking the previous two numbers of decoding
-	 * substrings.
-	 * For example, "112",
-	 */
+    /**
+     * Scan from the last char to the first and keep
+     * tracking the previous two numbers of decoding
+     * substrings.
+     * For example, "112",
+     */
     int numDecodings(string s) {
-    	if (s.empty()) {
-    		return 0;
-    	}
-    	int d = 0, d1 =0, d2 =1;
-    	int i = s.size();
-    	while (i--) {
-    		if (s[i] == '0') { // The substring is not valid with zero as the first letter
-    			d = 0;
-    		} else if (stoi(s.substr(i, 2)) <= 26) { // The substring can be separated
-    			d = d1 + d2;
-    		} else { // The substring cannot be separated
-    			d = d2;
-    		}
-			d1 = d2;
-			d2 = d;
-    	}
+        if (s.empty()) {
+            return 0;
+        }
+        int d = 0, d1 =0, d2 =1;
+        int i = s.size();
+        while (i--) {
+            if (s[i] == '0') { // The substring is not valid with zero as the first letter
+                d = 0;
+            } else if (stoi(s.substr(i, 2)) <= 26) { // The substring can be separated
+                d = d1 + d2;
+            } else { // The substring cannot be separated
+                d = d2;
+            }
+            d1 = d2;
+            d2 = d;
+        }
 
-    	return d;
+        return d;
     }
 };
 
 int main()
 {
-	string str = "132";
-	Solution sol;
-	int num = sol.numDecodings(str);
-	cout << "Number of decoding ways = " << num << endl;
-	return 0;
+    string str = "132";
+    Solution sol;
+    int num = sol.numDecodings(str);
+    cout << "Number of decoding ways = " << num << endl;
+    return 0;
 }
