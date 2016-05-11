18b0b24c474e8809817b359b1a9b23713a28339c
reminisce <wujun.nju@gmail.com>
2016-05-11 07:47:07 -0700

    Added one more solution to permutations

--- c++/permutations.cpp
+++ c++/permutations.cpp
@@ -39,13 +39,34 @@ public:
             }
         }
     }
+
+    vector<vector<int>> permute2(vector<int>& nums) {
+        vector<vector<int>> rs;
+        if (nums.empty()) return rs;
+        vector<int> p(nums);
+        permute2(nums, rs, p, 0);
+        return rs;
+    }
+
+    void permute2(vector<int>& nums, vector<vector<int>>& rs, vector<int>& p, int index) {
+        if (index == nums.size()) {
+            rs.push_back(p);
+            return;
+        }
+
+        for (int i = index; i < nums.size(); ++i) {
+            swap(p[index], p[i]);
+            permute2(nums, rs, p, index+1);
+            swap(p[index], p[i]);
+        }
+    }
 };
 
 int main()
 {
     vector<int> nums = {1, 2, 3};
     Solution sol;
-    vector<vector<int>> rs = sol.permute(nums);
+    vector<vector<int>> rs = sol.permute2(nums);
     for (vector<int>& r : rs) {
         for (int v : r) {
             cout << v << ' ';
