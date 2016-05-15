706e5b9587857b0c1c95776bd0619c8c5ef79cf5
reminisce <wujun.nju@gmail.com>
2016-05-15 13:07:45 -0700

    Added search in rotated sorted array ii

---
+++ c++/search_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,37 @@
+/*
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+*/
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = (int)nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[mid] == nums[left]) {
+                ++left;
+            } else if (nums[mid] > nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
