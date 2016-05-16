0491cd9869979a83abd775b9fbf639024771d45b
reminisce <wujun.nju@gmail.com>
2016-05-15 17:10:47 -0700

    Added flatten 2d vector

--- c++/flatten_2d_vector.cpp
+++ c++/flatten_2d_vector.cpp
@@ -11,6 +11,18 @@ Given 2d vector =
 ]
 By calling next repeatedly until hasNext returns false,
 the order of elements returned by next should be: [1, 2, 3, 4, 5, 6].
+
+Hint:
+
+How many variables do you need to keep track?
+Two variables is all you need. Try with x and y.
+Beware of empty rows. It could be the first few rows.
+To write correct code, think about the invariant to maintain. What is it?
+The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
+Not sure? Think about how you would implement hasNext(). Which is more complex?
+Common logic in two different places should be refactored into a common method.
+Follow up:
+As an added challenge, try to code it using only iterators in C++ or iterators in Java.
 */
 
 #include <vector>
@@ -25,41 +37,61 @@ public:
     typedef vector<vector_type> vector_2d_type;
     typedef vector_2d_type::iterator iterator_2d_type;
 
-    Vector2D(vector<vector<int>>& vec2d) : m_vec2d(vec2d), m_row(-1), m_col(-1) {
-        if (!m_vec2d.empty()) {
-            m_row = 0;
-        }
-        skipEmptyVector();
-        if (m_row != (int)m_vec2d.size()) {
-            m_col = 0;
-        }
+    Vector2D(vector<vector<int>>& vec2d) : m_vec2d(vec2d), m_row(0), m_col(0) {
     }
 
     int next() {
-        int val = m_vec2d[m_row][m_col];
-        if (++m_col == (int)m_vec2d[m_row].size()) {
-            ++m_row;
-            m_col = 0;
-        }
-        return val;
+        return m_vec2d[m_row][m_col++];
     }
 
     bool hasNext() {
-        if (m_row == -1 || m_col == -1 || m_row == (int)m_vec2d.size()) return false;
-        skipEmptyVector();
-        if (m_row == (int)m_vec2d.size()) return false;
-        return true;
-    }
+        while (m_row < (int)m_vec2d.size() && m_col == (int)m_vec2d[m_row].size()) {
+            ++m_row;
+            m_col = 0;
+        }
 
-private:
-    void skipEmptyVector() {
-        while (m_row < (int)m_vec2d.size() && m_vec2d[m_row].empty()) ++m_row;
+        return m_row < (int)m_vec2d.size();
     }
 
 private:
     vector_2d_type& m_vec2d;
     int m_row;
     int m_col;
+};
+
+class Vector2DIter {
+public:
+    typedef vector<int> vector_type;
+    typedef vector_type::iterator iterator_type;
+    typedef vector<vector_type> vector_2d_type;
+    typedef vector_2d_type::iterator iterator_2d_type;
+
+    Vector2DIter(vector<vector<int>>& vec2d) : m_vec2d(vec2d), m_row_iter(m_vec2d.begin()) {
+        if (!m_vec2d.empty()) {
+            m_col_iter = m_row_iter->begin();
+        }
+    }
+
+    int next() {
+        return *(m_col_iter++);
+    }
+
+    bool hasNext() {
+        if (m_vec2d.empty()) return false;
+
+        while (m_row_iter != m_vec2d.end() && m_col_iter == m_row_iter->end()) {
+            ++m_row_iter;
+            if (m_row_iter == m_vec2d.end()) return false;
+            m_col_iter = m_row_iter->begin();
+        }
+
+        return true;
+    }
+
+private:
+    vector_2d_type& m_vec2d;
+    iterator_2d_type m_row_iter;
+    iterator_type m_col_iter;
 };
 
 int main()
@@ -86,7 +118,7 @@ int main()
     vs.push_back(v4);
     vs.push_back(v4);
 
-    Vector2D v2d(vs);
+    Vector2DIter v2d(vs);
     while (v2d.hasNext()) {
         cout << v2d.next() << ' ';
     }
