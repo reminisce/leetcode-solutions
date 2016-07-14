4d5f82c87ead4feebc94b57d7ae61986c0644227
reminisce <wujun.nju@gmail.com>
2016-07-14 10:42:55 -0700

    Added search matrix 2d

---
+++ c++/SearchMatrix2DMatrixII.java
@@ -0,0 +1,38 @@
+/**
+ * Created on 7/14/16.
+ * Write an efficient algorithm that
+ * searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * For example,
+ *
+ * Consider the following matrix:
+ *
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * Given target = 5, return true.
+ *
+ * Given target = 20, return false.
+ */
+
+public class SearchMatrix2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (null == matrix || matrix.length == 0 || matrix[0].length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i <= m - 1 && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] < target) ++i;
+            else --j;
+        }
+        return false;
+    }
+}
