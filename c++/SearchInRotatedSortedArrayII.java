2553966102cc678fd967e95943ce552e5408b0de
reminisce <wujun.nju@gmail.com>
2016-07-27 22:18:02 -0700

    Added search in rotated sorted array ii

---
+++ c++/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,34 @@
+/**
+ * Created on 7/27/16.
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ *
+ * Would this affect the run-time complexity? How and why?
+ *
+ * Write a function to determine if a given target is in the array.
+ */
+
+public class SearchInRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] == nums[low]) ++low;
+            else if (nums[mid] > nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else  {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
