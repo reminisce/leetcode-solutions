c2e73fe97b35bc8df3a07711bee5e0bcc16a0f32
reminisce <wujun.nju@gmail.com>
2016-08-03 09:51:26 -0700

    Added binary tree longest consecutive sequence

--- c++/binary_tree_longest_consecutive_sequence.cpp
+++ c++/binary_tree_longest_consecutive_sequence.cpp
@@ -27,6 +27,7 @@ Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
 */
 
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -39,8 +40,63 @@ struct TreeNode {
 
 class Solution {
 public:
+
+    /**
+     * Post order traverse the tree. During the process
+     * update the max length. No cache is needed.
+     * If doing DFS, a cache is needed.
+     */
+    int longestConsecutivePostOrder(TreeNode* root) {
+        int maxLength = 0;
+        longestConsecutivePostOrder(root, maxLength);
+        return maxLength;
+    }
+
+    // return the consecutive length of the current node
+    int longestConsecutivePostOrder(TreeNode* node, int& maxLength) {
+        if (!node) return 0;
+        int leftLength = longestConsecutivePostOrder(node->left, maxLength);
+        int rightLength = longestConsecutivePostOrder(node->right, maxLength);
+        int curLength = 1;
+        if (node->left && node->val + 1 == node->left->val) {
+            curLength = max(curLength, leftLength+1);
+        }
+        if (node->right && node->val + 1 == node->right->val) {
+            curLength = max(curLength, rightLength+1);
+        }
+        maxLength = max(curLength, max(maxLength, max(leftLength, rightLength)));
+        return curLength;
+    }
+
     int longestConsecutive(TreeNode* root) { 
-        return longestConsecutive(root, nullptr, 0);
+        // return longestConsecutive(root, nullptr, 0);
+        unordered_map<TreeNode*, int> cache;
+        int maxLength = 0;
+        dfs(root, cache, maxLength);
+        return maxLength;
+    }
+
+    // DFS
+    void dfs(TreeNode* node, unordered_map<TreeNode*, int>& cache, int& maxLength) {
+        if (!node) return;
+        maxLength = max(maxLength, longestConsecutive2(node, cache));
+        dfs(node->left, cache, maxLength);
+        dfs(node->right, cache, maxLength);
+    }
+
+    int longestConsecutive2(TreeNode* node, unordered_map<TreeNode*, int>& cache) {
+        if (!node) return 0;
+        auto it = cache.find(node);
+        if (it != cache.end()) return it->second;
+        int len = 0;
+        if (node->left && node->val + 1 == node->left->val) {
+            len = max(longestConsecutive2(node->left, cache), len);
+        }
+        if (node->right && node->val + 1 == node->right->val) {
+            len = max(longestConsecutive2(node->right, cache), len);
+        }
+        cache[node] = len + 1;
+        return len + 1;
     }
 
     int longestConsecutive(TreeNode* curNode, TreeNode* parentNode, int len) {
@@ -65,7 +121,8 @@ int main()
     root->right->right->right = node;
 
     Solution sol;
-    int len = sol.longestConsecutive(root);
+    // int len = sol.longestConsecutive(root);
+    int len = sol.longestConsecutivePostOrder(root);
     cout << "Longest consecutive path is " << len << endl;
     return 0;
 }
