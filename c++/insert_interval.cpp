01bbcb611f9d66d0cc98ff6ac7019ad3d522d4f5
reminisce <wujun.nju@gmail.com>
2016-08-06 09:52:07 -0700

    Added insert interval

--- c++/insert_interval.cpp
+++ c++/insert_interval.cpp
@@ -1,97 +1,72 @@
-/* Given a set of non-overlapping intervals, insert a new interval into the
- * intervals (merge if necessary). You may assume that the intervals were
- * initially sorted according to their start times.
- * Example 1:
- * Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
- * Example 2:
- * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],
- * [3,10],[12,16].
- * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
- */
+/*
+Given a set of non-overlapping intervals,
+insert a new interval into the intervals
+(merge if necessary).
 
-#include <cstdlib>
+You may assume that the intervals were
+initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge
+[2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16],
+insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9]
+overlaps with [3,5],[6,7],[8,10].
+*/
+
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-
-//  Definition for an interval.
 struct Interval {
     int start;
     int end;
-
     Interval() : start(0), end(0) {}
-
     Interval(int s, int e) : start(s), end(e) {}
 };
 
 class Solution {
 public:
     vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
-        int n = intervals.size();
-        
-        vector<Interval> res; // result
-        
-        // Find rightmost interval whose start is not greater than newInterval.start
-        int left = 0, right = n-1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (newInterval.start < intervals[mid].start) {
-                right = mid - 1;
-            }
+        vector<Interval> res;
+        size_t i = 0;
+        while (i < intervals.size()) {
+            if (intervals[i].start > newInterval.start) break;
+            res.push_back(intervals[i++]);
+        }
+
+        if (res.empty() || res.back().end < newInterval.start) res.push_back(newInterval);
+        else {
+            res.back().end = max(newInterval.end, res.back().end);
+        }
+
+        while (i < intervals.size()) {
+            if (res.back().end < intervals[i].start) res.push_back(intervals[i]);
             else {
-                left = mid + 1;
+                res.back().end = max(intervals[i].end, res.back().end);
             }
+            ++i;
         }
-        int left_end = right;
-        
-        // Find leftmost interval whose end is not less than newInterval.end
-        left = 0;
-        right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (newInterval.end > intervals[mid].end) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-        int right_end = left;
-        
-        // Merge newInterval and intervals[left_end], ..., intervals[right_end]
-        // if necessary.
-        if (left_end >= 0 && intervals[left_end].end >= newInterval.start) {
-            newInterval.start = intervals[left_end--].start;
-        }
-        
-        if (right_end < n && intervals[right_end].start <= newInterval.end) {
-            newInterval.end = intervals[right_end++].end;
-        }
-        
-        res.reserve(2+left_end+n-right_end);
-        
-        for (int i = 0; i <= left_end; ++i) {
-            res.push_back(intervals[i]);
-        }
-        
-        res.push_back(newInterval);
-        
-        for (int i = right_end; i < n; ++i) {
-            res.push_back(intervals[i]);
-        }
-        
+
         return res;
     }
 };
 
-int main(int argc, char** argv) {
+int main()
+{
     vector<Interval> intervals;
-    intervals.push_back(Interval(1, 5));
-    intervals.push_back(Interval(6, 8));
-    Interval newInterval(5, 6);
+    intervals.push_back(Interval(1, 3));
+    intervals.push_back(Interval(6, 9));
+    Interval newInterval(2, 5);
     Solution sol;
-    intervals = sol.insert(intervals, newInterval);
-    return 0;
+    vector<Interval> res = sol.insert(intervals, newInterval);
+    for (auto& interval : res) {
+        cout << '[' << interval.start << ", " << interval.end << "] ";
+    }
+    cout << endl;
 }
-
