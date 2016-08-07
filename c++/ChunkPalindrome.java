e815e50969fd22560bec0b4b43d35ea476b5d56a
reminisce <wujun.nju@gmail.com>
2016-08-07 00:08:17 -0700

    Added chunk palindrome

--- c++/ChunkPalindrome.java
+++ c++/ChunkPalindrome.java
@@ -7,12 +7,13 @@
 public class ChunkPalindrome {
 
     public static void main(String[] args) {
-        String s = "volvo";
+        String s = "aabba";
         ChunkPalindrome app = new ChunkPalindrome();
-        System.out.println(app.countChunk(s));
+        System.out.println("Recursive: " + app.countChunkRecursive(s));
+        System.out.println("Iterative: " + app.countChunkIterative(s));
     }
 
-    public int countChunk(String s) {
+    public int countChunkRecursive(String s) {
         if (null == s) return 0;
         if (s.length() == 0 || s.length() == 1) return s.length();
 
@@ -20,9 +21,41 @@ public class ChunkPalindrome {
             String left = s.substring(0, i+1);
             String right = s.substring(s.length()-i-1);
             if (left.equals(right)) {
-                return 2 + countChunk(s.substring(i+1, s.length()-i-1));
+                return 2 + countChunkRecursive(s.substring(i+1, s.length()-i-1));
             }
         }
         return 1;
+    }
+
+    public int countChunkIterative(String s) {
+        if (null == s) return 0;
+        int left = 0, right = s.length() - 1;
+        int count = 0;
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                count += 2;
+                ++left;
+                --right;
+            } else {
+                int i = left + 1, j = right - 1;
+                while (i < j) {
+                    if (s.substring(left, i+1).equals(s.substring(j, right+1))) {
+                        count += 2;
+                        left = i + 1;
+                        right = j - 1;
+                        break;
+                    } else {
+                        ++i;
+                        --j;
+                    }
+                }
+                if (i >= j) {
+                    ++count;
+                    break;
+                }
+            }
+        }
+        if (left == right) ++count;
+        return count;
     }
 }
