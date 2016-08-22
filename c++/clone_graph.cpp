7648f02b6f0b5c74b6e0ba2db131596c651771c4
reminisce <wujun.nju@gmail.com>
2016-08-21 17:19:16 -0700

    Added clone graph

--- c++/clone_graph.cpp
+++ c++/clone_graph.cpp
@@ -15,38 +15,49 @@ Second node is labeled as 1. Connect node 1 to node 2.
 Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
 Visually, the graph looks like the following:
 
-       1
-      / \
-     /   \
-    0 --- 2
-         / \
-         \_/
+   1
+  / \
+ /   \
+0 --- 2
+     / \
+     \_/
 */
 
 /**
- * Definition for undirected graph.
- * struct UndirectedGraphNode {
- *     int label;
- *     vector<UndirectedGraphNode *> neighbors;
- *     UndirectedGraphNode(int x) : label(x) {};
- * };
- */
+* Definition for undirected graph.
+* struct UndirectedGraphNode {
+*     int label;
+*     vector<UndirectedGraphNode *> neighbors;
+*     UndirectedGraphNode(int x) : label(x) {};
+* };
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct UndirectedGraphNode {
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
+};
+
 class Solution {
 public:
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+    UndirectedGraphNode *cloneGraphBFS(UndirectedGraphNode *node) {
         if (!node) return node;
         // Keep a mapping between the original node and the newly cloned node.
         // Also acts as a indicator whether the node has been visited.
         unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> nodeMap;
-        UndirectedGraphNode* newNode = new UndirectedGraphNode(node->label);
-        nodeMap[node] = newNode;
         queue<UndirectedGraphNode*> q;
         q.push(node);
         while (!q.empty()) {
             UndirectedGraphNode* curNode = q.front();
             q.pop();
-            auto it = nodeMap.find(curNode);
-            if (it == nodeMap.end()) {
+            if (!nodeMap.count(curNode)) {
                 nodeMap[curNode] = new UndirectedGraphNode(curNode->label);
             }
             for (UndirectedGraphNode* neighborNode : curNode->neighbors) {
@@ -58,6 +69,32 @@ public:
             }
         }
 
-        return newNode;
+        return nodeMap[node];
+    }
+
+    // clone graph using DFS
+    UndirectedGraphNode *cloneGraphDFS(UndirectedGraphNode *node) {
+        if (!node) return node;
+        stack<UndirectedGraphNode*> graphNodeStack;
+        graphNodeStack.push(node);
+
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> graphNodeMap;
+        while (!graphNodeStack.empty()) {
+            UndirectedGraphNode* curNode = graphNodeStack.top();
+            graphNodeStack.pop();
+            if (!graphNodeMap.count(curNode)) {
+                graphNodeMap[curNode] = new UndirectedGraphNode(curNode->label);
+            }
+            for (int i = 0; i < curNode->neighbors.size(); ++i) {
+                UndirectedGraphNode* neighbor = curNode->neighbors[i];
+                if (!graphNodeMap.count(neighbor)) {
+                    graphNodeStack.push(neighbor);
+                    graphNodeMap[neighbor] = new UndirectedGraphNode(neighbor->label);
+                }
+                graphNodeMap[curNode]->neighbors.push_back(graphNodeMap[neighbor]);
+            }
+        }
+
+        return graphNodeMap[node];
     }
 };
