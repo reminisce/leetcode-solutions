28770cb51ae10321a2d26a24b04fcf991cde57e6
reminisce <wujun.nju@gmail.com>
2016-08-23 23:26:59 -0700

    Added word ladder to get transformation path

--- c++/WordLadder.java
+++ c++/WordLadder.java
@@ -1,7 +1,4 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created on 7/28/16.
@@ -36,8 +33,11 @@ public class WordLadder {
         wordList.add("dog");
         wordList.add("lot");
         wordList.add("log");
+        wordList.add("hog");
+        wordList.add("lit");
         WordLadder app = new WordLadder();
-        System.out.println(app.ladderLength(beginWord, endWord, wordList));
+        // System.out.println(app.ladderLength(beginWord, endWord, wordList));
+        System.out.println(app.ladderPath(beginWord, endWord, wordList).toString());
     }
 
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
@@ -67,5 +67,49 @@ public class WordLadder {
             }
         }
         return 0;
+    }
+
+    public List<String> ladderPath(String beginWord, String endWord, Set<String> wordList) {
+        if (beginWord .equals(endWord)) {
+            List<String> res = new ArrayList<>();
+            res.add(beginWord);
+            res.add(endWord);
+            return res;
+        }
+
+        Queue<List<String>> ladderQueue = new LinkedList<>();
+        List<String> firstLadder = new ArrayList<>();
+        firstLadder.add(beginWord);
+        ladderQueue.offer(firstLadder);
+
+        while (!ladderQueue.isEmpty()) {
+            int sz = ladderQueue.size();
+            for (int i = 0; i < sz; ++i) {
+                List<String> curLadder = ladderQueue.poll();
+                System.out.println(curLadder.toString());
+                StringBuilder lastWord = new StringBuilder(curLadder.get(curLadder.size() - 1));
+                for (int j = 0; j < lastWord.length(); ++j) {
+                    char originalChar = lastWord.charAt(j);
+                    for (int k = 0; k < 26; ++k) {
+                        char changedChar = (char)(k + 'a');
+                        if (changedChar == originalChar) continue;
+                        lastWord.setCharAt(j, changedChar);
+                        String newWord = lastWord.toString();
+                        lastWord.setCharAt(j, originalChar);
+                        if (newWord.equals(endWord)) {
+                            curLadder.add(newWord);
+                            return curLadder;
+                        }
+                        if (!wordList.contains(newWord)) continue;
+                        wordList.remove(newWord);
+                        List<String> nextLadder = new ArrayList<>(curLadder);
+                        nextLadder.add(newWord);
+                        ladderQueue.offer(nextLadder);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>();
     }
 }
