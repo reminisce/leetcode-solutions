4bc48339c8e976d85524322caff405ff144bd1e6
reminisce <wujun.nju@gmail.com>
2016-08-25 15:00:21 -0700

    Added flatten binary tree to linked list

--- c++/FlattenBinaryTreeToLinkedList.java
+++ c++/FlattenBinaryTreeToLinkedList.java
@@ -31,6 +31,36 @@
  */
 
 public class FlattenBinaryTreeToLinkedList {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        FlattenBinaryTreeToLinkedList app = new FlattenBinaryTreeToLinkedList();
+        app.flatten2(root);
+        TreeNode p = root;
+        TreeNode q = null;
+        while (p != null) {
+            System.out.print(p.val + " ");
+            q = p;
+            p = p.right;
+        }
+        System.out.println();
+
+        while (q != null) {
+            System.out.print(q.val + " ");
+            p = q;
+            q = q.left;
+        }
+    }
+
+    /**
+     * Flatten the tree to a singly linked list
+     * @param root
+     */
     public void flatten(TreeNode root) {
         if (null == root) return;
         TreeNode curNode = root;
@@ -42,6 +72,27 @@ public class FlattenBinaryTreeToLinkedList {
                 curNode.right = curNode.left;
                 curNode.left = null;
             }
+            curNode = curNode.right;
+        }
+    }
+
+    /**
+     * Flatten the tree to a doubly linked list
+     * @param root
+     */
+    public void flatten2(TreeNode root) {
+        if (null == root) return;
+        TreeNode curNode = root;
+        TreeNode preNode = null;
+        while (curNode != null) {
+            if (curNode.left != null) {
+                TreeNode p = curNode.left;
+                while (p.right != null) p = p.right;
+                p.right = curNode.right;
+                curNode.right = curNode.left;
+            }
+            curNode.left = preNode;
+            preNode = curNode;
             curNode = curNode.right;
         }
     }
