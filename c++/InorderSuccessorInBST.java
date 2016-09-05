ff96781f35aa4f8af3ee645a846b02e9bd675c74
reminisce <wujun.nju@gmail.com>
2016-09-04 21:10:52 -0700

    Added inorder successor in bst

---
+++ c++/InorderSuccessorInBST.java
@@ -0,0 +1,25 @@
+/**
+ * Created on 9/4/16.
+ * Given a binary search tree and a node
+ * in it, find the in-order successor
+ * of that node in the BST.
+ *
+ * Note: If the given node has no
+ * in-order successor in the tree,
+ * return null.
+ */
+
+public class InorderSuccessorInBST {
+    TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode res = null;
+        while (root != null) {
+            if (root.val > p.val) {
+                res = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return res;
+    }
+}
